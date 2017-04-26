2f0d455c5e36bc1f6baf2e057f64f10a7d08bb08
Balaji Radhakrishnan <balag59@gmail.com>
2017-04-26 03:22:26 -0400

    added try except statements

--- server.py
+++ server.py
@@ -4,8 +4,12 @@ from threading import Thread
 def new_connection(client_socket):
     print('new connection')
     while(True):
-         data = client_socket.recv(1024)
-         client_socket.send(data)
+        try:
+            data = client_socket.recv(1024)
+            client_socket.send(data)
+        except:
+            break
+    print('closing client connection')
     client_socket.close()
 
 
@@ -15,11 +19,13 @@ host = socket.gethostname() # Get local machine name
 port = 7734                # Reserve a port for your service.
 server_socket.bind((host, port))        # Bind to the port
 
+
 server_socket.listen(5)                 # Now wait for client connection.
 print("Server is awaiting a connection")
-while True:
-   client_socket, client_addr = server_socket.accept()     # Establish connection with client.
-   print('Got connection from', client_addr)
-   new_thread = Thread(target=new_connection,args=(client_socket,))
-   new_thread.start()
+while(True):
+        client_socket, client_addr = server_socket.accept()     # Establish connection with client.
+        print('Got connection from', client_addr)
+        new_thread = Thread(target=new_connection,args=(client_socket,))
+        new_thread.start()
+print('shutting down server')
 server_socket.close()                # Close the connection
