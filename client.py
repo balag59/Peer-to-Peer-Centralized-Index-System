f4b571b5abb680c6bb6906f80140ea9ecc3781c3
bradhak <bradhak@ncsu.edu>
2017-04-26 15:18:44 -0400

    added methods to add rfc on join

--- client.py
+++ client.py
@@ -1,5 +1,7 @@
 import socket               # Import socket module
 from threading import Thread
+import os
+import glob
 
 #to listen to any peers requiring uploads
 def upload_server():
@@ -9,9 +11,9 @@ def upload_server():
 def send_requests(note, server_host, server_port):
     client_socket = socket.socket()
     client_socket.connect((server_host, server_port))
-    client_socket.send(note)
-    response = client_socket.recv(1024)
-    print('response :')
+    client_socket.send(note.encode())
+    response = client_socket.recv(1024).decode()
+    print('response from the central server :')
     print(response)
     client_socket.close()
 
@@ -21,9 +23,18 @@ def send_requests(note, server_host, server_port):
 def handle_input():
     server_host = input("enter the host of the central server: ")
     server_port = 7734
-    #for the first time peer joins the system
-    message = "JOIN P2P-CI/1.0 Host: "+host+" Port: "+str(port)
+    #for the first time a peer joins the system
+    note = "JOIN P2P-CI/1.0\nHost: "+host+'\n'+"Port: "+str(port)
     send_requests(note, server_host, server_port)
+    file_list = glob.glob('*.txt')
+    for file in file_list:
+        title = file.split('.')[0]
+        rfc = int(title.split('c')[1])
+        note = "ADD RFC " +str(rfc)+" P2P-CI/1.0\nHost: "+host+'\n'+"Port: "+str(port)+'\n'+"Title: "+title
+        send_requests(note, server_host, server_port)
+
+
+
 
 
 
