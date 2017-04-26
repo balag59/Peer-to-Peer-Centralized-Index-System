0d369a77735a033da672df4f3f7244f7f7dfce1d
Balaji Radhakrishnan <balag59@gmail.com>
2017-04-26 17:34:12 -0400

    added user options for peers

--- client.py
+++ client.py
@@ -2,6 +2,8 @@ import socket               # Import socket module
 from threading import Thread
 import os
 import glob
+import sys
+
 
 #to listen to any peers requiring uploads
 def upload_server():
@@ -17,7 +19,28 @@ def send_requests(note, server_host, server_port):
     print(response)
     client_socket.close()
 
+#send a request to list all rfc's
+def list_rfc(server_host, server_port):
+    note = "LIST ALL P2P-CI/1.0\nHost: "+host+'\n'+"Port: "+str(port)
+    send_requests(note, server_host, server_port)
 
+#accept input and send requests to add rfc's
+def add_rfc(server_host,server_port):
+    print('please enter a RFC number')
+    rfc = int(input())
+    print('please enter the RFC title')
+    title = input()
+    note = "ADD RFC " +str(rfc)+" P2P-CI/1.0\nHost: "+host+'\n'+"Port: "+str(port)+'\n'+"Title: "+title
+    send_requests(note, server_host, server_port)
+
+#accept input and send requests to find a rfc
+def lookup_rfc(server_host,server_port):
+    print('please enter a RFC number')
+    rfc = int(input())
+    print('please enter the RFC title')
+    title = input()
+    note = "LOOKUP RFC " +str(rfc)+" P2P-CI/1.0\nHost: "+host+'\n'+"Port: "+str(port)+'\n'+"Title: "+title
+    send_requests(note, server_host, server_port)
 
 #to handle any user input
 def handle_input():
@@ -32,14 +55,30 @@ def handle_input():
         rfc = int(title.split('c')[1])
         note = "ADD RFC " +str(rfc)+" P2P-CI/1.0\nHost: "+host+'\n'+"Port: "+str(port)+'\n'+"Title: "+title
         send_requests(note, server_host, server_port)
+    #after the client joins sucessfully give functionality options
+    while(True):
+        print("What do you want to do?Enter number corresponding to an option you choose")
+        print("1. Add RFC's")
+        print("2. Lookup RFC's")
+        print("3. List all available RFC's")
+        print("4. Download a RFC file")
+        print("5. Quit")
+        if(int(input()) == 1):
+            add_rfc(server_host, server_port)
+        elif(int(input()) == 2):
+            lookup_rfc(server_host, server_port)
+        elif(int(input()) == 3):
+            list_rfc(server_host, server_port)
+        elif(int(input()) == 4):
+            download_rfc(server_host, server_port)
+        elif(int(input()) == 5):
+            quit(server_host, server_port)
+        else:
+            print('please enter a valid choice')
 
+#main client functionality
 
-
-
-
-
-
-host = socket.gethostname() # Get local machine name
+host = input("please enter an unused host name: ")
 port = int(input("please enter an unused port number: "))
 upload_thread = Thread(target=upload_server)
 upload_thread.start()
