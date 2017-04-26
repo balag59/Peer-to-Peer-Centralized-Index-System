0cbfdb4974746e3ce0f0bf9f1af2740d1c945da2
bradhak <bradhak@ncsu.edu>
2017-04-26 06:26:08 -0400

    too sleepy

--- client.py
+++ client.py
@@ -1,18 +1,35 @@
 import socket               # Import socket module
+from threading import Thread
 
-client_socket = socket.socket()         # Create a socket object
+#to listen to any peers requiring uploads
+def upload_server():
+    pass
+
+#to send requests to the central server_host
+def send_requests(note, server_host, server_port):
+    client_socket = socket.socket()
+    client_socket.connect((server_host, server_port))
+    client_socket.send(note)
+    response = client_socket.recv(1024)
+    print('response :')
+    print(response)
+    client_socket.close()
+
+
+
+#to handle any user input
+def handle_input():
+    server_host = input("enter the host of the central server: ")
+    server_port = 7734
+    #for the first time peer joins the system
+    message = "JOIN P2P-CI/1.0 Host: "+host+" Port: "+str(port)
+    send_requests(note, server_host, server_port)
+
+
+
+
 host = socket.gethostname() # Get local machine name
-port = 7734               # Reserve a port for your service.
-
-client_socket.connect((host, port))
-
-while(True):
-    try:
-        data = input("message: ")
-        client_socket.send(data.encode())
-        response = client_socket.recv(1024)
-        print(response)
-    except:
-        break
-print('closing connection')
-client_socket.close()                     # Close the socket when done
+port = int(input("please enter an unused port number: "))
+upload_thread = Thread(target=upload_server)
+upload_thread.start()
+handle_input()
