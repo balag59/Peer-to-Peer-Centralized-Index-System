c0d32c3366ca96819fd61d8c01e86a48b8bf793a
bradhak <bradhak@ncsu.edu>
2017-04-26 03:22:26 -0400

    added try except statements

--- client.py
+++ client.py
@@ -5,9 +5,14 @@ host = socket.gethostname() # Get local machine name
 port = 7734               # Reserve a port for your service.
 
 client_socket.connect((host, port))
-while True:
-    data = input("message: ")
-    client_socket.send(data.encode())
-    response = client_socket.recv(1024)
-    print(response)
+
+while(True):
+    try:
+        data = input("message: ")
+        client_socket.send(data.encode())
+        response = client_socket.recv(1024)
+        print(response)
+    except:
+        break
+print('closing connection')
 client_socket.close()                     # Close the socket when done
